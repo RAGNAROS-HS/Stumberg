29059c6f61b5314bc88889c5d47ebbcf38033942
Hugo Sokołowski-Katzer <hugo.s.katzer@gmail.com>
2025-12-03 15:45:59 +0100

    working tool+agent with openai

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from langchain.chat_models import init_chat_model
+from langchain.agents import create_agent
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
-os.environ["PPLX_API_KEY"] = os.getenv("PERPLEXITY_KEY", "")
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY", "")
+#gpt-4.1-nano
 
-model = init_chat_model(
-    "sonar",
-    model_provider="perplexity",
-    temperature=0.0,
+def get_weather(city: str) -> str:
+    """Get weather for a given city."""
+    return f"It's always sunny in {city}!"
+
+#instead of this I can use the direct OPENAI package, need to check how that works
+agent = create_agent(
+    "gpt-4.1-nano",
+    tools=[get_weather]
 )
 
-print(model.invoke("Why do parrots talk?"))
+
+print(agent.invoke({"messages": [{"role": "user", "content": "what is the weather in Amsterdam?"}]}))
